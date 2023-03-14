1f6e58dd65194acd4853e92684c0e3237332a841
tuanpham <tuanp@galaxy.local.vn>
2023-03-14 14:08:34 +0700

    add gg seo

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -50,3 +50,12 @@ export default {
   name: 'NuxtTutorial'
 }
 </script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YB5VSEP985"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YB5VSEP985');
+</script>
